a0cf7bd6d1097a9f7696dca7b0f40b486c922d09
Johnatas Dias dos Santos <73427033+JOH-br@users.noreply.github.com>
2026-03-10 20:28:07 -0300

    Atualizar o calculadora.py
    fuctionalit add

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,20 @@
 # Calculadora funcional
+
+nmb1 = 1
+nmb2 = 1
+action = ""
+
+if action == "+":
+  result = round(float(nmb1),2) + round(float(nmb2),2)
+
+if action == "-":
+  result = round(float(nmb1),2) - round(float(nmb2),2)
+
+if action == "x":
+  result = round(float(nmb1),2) * round(float(nmb2),2)
+
+if action == "/":
+  if round(float(nmb2),2) == round(float(0),2):
+    print("you can divide by 0")
+    break
+  result = round(float(nmb1),2) / round(float(nmb2),2)
